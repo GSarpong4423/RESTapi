2c8f7764ebe60793109d9e059d08fa0e5036836a
GSarpong4423 <sarpongg@gmail.com>
2019-02-03 16:21:25 +0000

    creating react

--- routes/api.js
+++ routes/api.js
@@ -1,31 +1,52 @@
 const express = require('express');
 const router = express.Router();
+const farmer = require('../models/farmer');
 
-//get a list of the ninjas from db
-router.get('/ninjas', function(req, res){
-    res.send ({type: 'GET'});
+// GET request
+router.get("/farmers", function(req, res, next) {
+  farmer.aggregate([{
+    $geoNear: {
+      near: {
+        type: "Point",
+        coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]
+      },
+      distanceField: "dist.calculated",
+      includeLocs: "dist.location",
+      maxDistance: 100000,
+      spherical: true
+    }
+  }])
+    .then(function(farmer) {
+      res.send(farmer);
+    })
+    .catch(next);
 });
 
-//add a new ninja to database
-router.post('/ninjas', function(req, res){
-    console.log(req.body);
-    res.send ({
-        type: 'POST',
-        name: req.body.name,
-        rank: req.body.rank
+
+// POST request | add a new farmer to the db
+router.post('/farmers', function(req, res, next) {
+    farmer.create(req.body).then(function(farmer) {
+        res.send(farmer);
+    }).catch(next);
+});
+
+// UPDATE (put) request to put a new record in the db/collection
+router.put('/farmers/:id', function(req, res, next) {
+    farmer.findByIdAndUpdate({_id:req.params.id}, req.body).then(function() {
+        farmer.findOne({_id:req.params.id}).then(function(farmer) {
+           res.send(farmer);  
+        });
+        
+    });
+   
+});
+
+// DELETE request
+router.delete('/farmers/:id', function(req, res, next) {
+    farmer.findByIdAndRemove({_id:req.params.id}).then(function(farmer) {
+        res.send(farmer);
     });
 });
-
-//update ninja in database
-router.put('/ninjas/id', function(req, res){
-    res.send ({type: 'PUT'});
-});
-
-//delete ninga in database
-router.delete('/ninjas/:id', function(req, res){
-    res.send ({type: 'DELETE'});
-});
-
 
 
 module.exports = router;
