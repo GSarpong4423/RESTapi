8e2e7266894aec9b8145e955500ffd8f0785c7fa
GSarpong4423 <sarpongg@gmail.com>
2019-02-03 15:38:28 +0000

    get request

--- routes/api.js
+++ routes/api.js
@@ -2,34 +2,53 @@ const express = require('express');
 const router = express.Router();
 const Ninja = require('../models/ninja');
 
-//get a list of the ninjas from db
-router.get('/ninjas', function(req, res, next){
-    res.send ({type: 'GET'});
+// GET request
+router.get("/ninjas", function(req, res, next) {
+  Ninja.aggregate([{
+    $geoNear: {
+      near: {
+        type: "Point",
+        coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]
+      },
+      distanceField: "dist.calculated",
+      includeLocs: "dist.location",
+      maxDistance: 100000,
+      spherical: true
+    }
+  }])
+    .then(function(ninja) {
+      res.send(ninja);
+    })
+    .catch(next);
 });
 
-//add a new ninja to database
-router.post('/ninjas', function(req, res, next){
-    Ninja.create(req.body).then(function(ninja){
-            res.send(ninja);
+
+// POST request | add a new ninja to the db
+router.post('/ninjas', function(req, res, next) {
+    Ninja.create(req.body).then(function(ninja) {
+        res.send(ninja);
     }).catch(next);
 });
 
-//update ninja in database
-router.put('/ninjas/id', function(req, res, next){
-    Ninja.findByIdAndRemove({_id:req.params.id},req.body).then(function(){
-        Ninja.findOne({_id:req.params.id}).then(function(ninja){
-            res.send (ninja);
+// UPDATE (put) request to put a new record in the db/collection
+router.put('/ninjas/:id', function(req, res, next) {
+    Ninja.findByIdAndUpdate({_id:req.params.id}, req.body).then(function() {
+        Ninja.findOne({_id:req.params.id}).then(function(ninja) {
+           res.send(ninja);  
         });
+        
     });
+   
 });
 
-//delete ninga in database
-router.delete('/ninjas/:id', function(req, res, next){
-    Ninja.findByIdAndRemove({_id:req.params.id}).then(function(ninja){
+// DELETE request
+router.delete('/ninjas/:id', function(req, res, next) {
+    Ninja.findByIdAndRemove({_id:req.params.id}).then(function(ninja) {
         res.send(ninja);
     });
 });
 
 
-
 module.exports = router;
+
+
